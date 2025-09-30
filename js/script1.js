8274997d2142743723f2d9b50e34fcfb20afcab2
Denis <145887997+GithubDenis5@users.noreply.github.com>
2025-09-30 10:56:56 +0300

    add page (#5)
    * feat: add html
    
    * feat: add style with css
    
    * feat: add js
    
    * style: fix folders

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const colorBtn = document.getElementById("colorBtn");
+
+  colorBtn.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    document.body.style.color = randomColor;
+  });
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
